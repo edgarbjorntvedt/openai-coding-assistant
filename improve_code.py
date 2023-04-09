a182e7cf8174e93bfb9526ffeee3e91636b7330a
Edgar Bjørntvedt <edgar.bjorntvedt@gmail.com>
2023-04-10 01:25:35 +0200

    added debug stats logging moved system messages to a separate file

--- improve_code.py
+++ improve_code.py
@@ -1,33 +1,70 @@
-import openai
+import requests
+import json
 from dotenv import dotenv_values
 
 # Load the OpenAI API key and other configurations from the .env file
 config = dotenv_values(".env")
-openai.api_key = config["OPENAI_API_KEY"]
-model = config["OPENAI_MODEL"]
-filename = config["FILENAME"]
+api_key = config["OPENAI_API_KEY"]
+filename = sys.argv[1] if len(sys.argv) > 1 else config["FILENAME"]
+system_message_file = config["SYSTEM_MESSAGE_FILE"]
 
 # Load the content of the file index.mjs
 with open(filename, "r") as file:
     code = file.read()
 
-# Send the code to OpenAI for code improvements
-response = openai.Completion.create(
-#     model=model,
-    engine=model,
-    prompt=code,
-    max_tokens=100,
-    n=1,
-    stop=None,
-    temperature=0.7,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-)
+# Set up the API endpoint URL
+url = "https://api.openai.com/v1/chat/completions"
 
-# Extract the improved code from the OpenAI response
-improved_code = response.choices[0].text
+# Set up the request headers
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}",
+}
+# Load the content of the system message file
+with open(system_message_file, 'r') as f:
+    system_message = f.read().strip()
 
-# Write the improved code to the file openai.index.mjs
-with open("openai." + filename, "w") as file:
+token_length = int(len(code + ' ' + system_message)*1.1/4)
+print("guessed token_length: ", token_length)
+
+# Set up the request data
+data = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+        {
+            "role": "system",
+            "content": system_message
+        },
+        {
+            "role": "user",
+            "content":code
+        },
+        ],
+    "temperature": 0.2,
+    "max_tokens": 4097 - token_length,
+}
+
+# Send the request to the API
+response = requests.post(url, headers=headers, data=json.dumps(data))
+
+# Parse the response JSON
+response_json = json.loads(response.text)
+
+# deep copy the response
+new_response = json.loads(json.dumps(response_json))
+# remove the content field from each message
+for choice in new_response['choices']:
+    choice['message']['content'] = '...'
+
+# print the modified response
+print(json.dumps(new_response, indent=2))
+
+# Extract the generated text from the response
+improved_code = response_json["choices"][0]["message"]["content"]
+
+# Backup the original file
+os.rename(filename, filename + ".bck")
+
+# Write the improved code to the file with the same filename as the original file
+with open(filename, "w") as file:
     file.write(improved_code)
