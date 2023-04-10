00ad2f997f89df1f6421322b06805c1703517e20
Edgar Bjørntvedt <edgar.bjorntvedt@gmail.com>
2023-04-10 14:21:07 +0200

    added support for logs adjustments

--- improve_code.py
+++ improve_code.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser(description='Improve code using OpenAI GPT-3')
 parser.add_argument('--filename', type=str, help='Filename of the code to be improved', required=True)
 parser.add_argument('--key', type=str, help='API key for OpenAI', required=True)
 parser.add_argument('--system-message-file', type=str, help='Filename of the system message', required=True)
+parser.add_argument('--logs', choices=['verbose', 'info', 'off'], help='Logging level', default='info')
 args = parser.parse_args()
 
 with open(args.filename) as f:
@@ -16,7 +17,6 @@ with open(args.system_message_file) as f:
     system_message = f.read().strip()
 
 token_length = int(len(code + ' ' + system_message) * 1.1 / 4)
-print(f"guessed token_length: {token_length}")
 
 response = requests.post(
     'https://api.openai.com/v1/chat/completions',
@@ -31,16 +31,11 @@ response = requests.post(
             {'role': 'user', 'content': code},
         ],
         'temperature': 0.2,
-        'max_tokens': 4097 - token_length,
+        'max_tokens': 4097 - token_length - 50,
     }
 )
 
 response_json = response.json()
-
-new_response = json.loads(json.dumps(response_json))
-for choice in new_response['choices']:
-    choice['message']['content'] = '...'
-print(json.dumps(new_response, indent=2))
 
 improved_code = response_json["choices"][0]["message"]["content"]
 
@@ -48,3 +43,14 @@ os.rename(args.filename, f'{args.filename}.bck')
 
 with open(args.filename, 'w') as f:
     f.write(improved_code)
+
+if args.logs in ['verbose']:
+    print(f"System message file: {args.system_message_file}")
+    print(f"Guessed token_length: {token_length}")
+    new_response = json.loads(json.dumps(response_json))
+    for choice in new_response['choices']:
+        choice['message']['content'] = '...'
+    print(json.dumps(new_response, indent=2))
+
+if args.logs in ['info', 'verbose']:
+    print(f"Improved file: {args.filename}")
